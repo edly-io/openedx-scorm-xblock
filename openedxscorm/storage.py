47870c9d775177b75a25193735f04e4ce82f8594
nauman4386 <noman.shafi@edly.io>
2023-05-25 19:04:02 +0500

    resolve aws custom domain issue

--- openedxscorm/storage.py
+++ openedxscorm/storage.py
@@ -16,12 +16,12 @@ class S3ScormStorage(S3Boto3Storage):
         super().__init__(bucket=bucket, querystring_auth=querystring_auth,
                          querystring_expire=querystring_expire)
 
-    def url(self, name):
+    def url(self, name, parameters=None, expire=None):
         """
         Override url method of S3Boto3Storage
         """
         if not self.querystring_auth:
-            return super().url(name)
+            return self.generate_url(name, parameters, expire)
 
         if name.startswith(self.xblock.extract_folder_path):
             handler_url = self.xblock.runtime.handler_url(self.xblock, 'assets_proxy')
@@ -36,7 +36,25 @@ class S3ScormStorage(S3Boto3Storage):
             # construct the URL for proxy function
             return f'{handler_url}{self.xblock.index_page_path}'
 
-        return super().url(os.path.join(self.xblock.extract_folder_path, name))
+        return self.generate_url(os.path.join(self.xblock.extract_folder_path, name), parameters, expire)
+
+    def generate_url(self, name, parameters, expire):
+        """
+        Generate a URL either with or without querystring authentication
+        """
+        # Preserve the trailing slash after normalizing the path.
+        name = self._normalize_name(self._clean_name(name))
+        if expire is None:
+            expire = self.querystring_expire
+
+        params = parameters.copy() if parameters else {}
+        params['Bucket'] = self.bucket.name
+        params['Key'] = self._encode_name(name)
+        url = self.bucket.meta.client.generate_presigned_url('get_object', Params=params,
+                                                             ExpiresIn=expire)
+        if self.querystring_auth:
+            return url
+        return self._strip_signing_parameters(url)
 
 
 def s3(xblock):
@@ -54,7 +72,7 @@ def s3(xblock):
     Returns:
         S3ScormStorage: An instance of the S3ScormStorage class.
     """
-    bucket = xblock.xblock_settings.get('SCORM_S3_BUCKET_NAME', "ScormS3Bucket")
+    bucket = xblock.xblock_settings.get('SCORM_S3_BUCKET_NAME', "DEFAULT_S3_BUCKET_NAME")
     querystring_auth = xblock.xblock_settings.get('SCORM_S3_QUERY_AUTH', True)
     querystring_expire = xblock.xblock_settings.get('SCORM_S3_EXPIRES_IN', 604800)
     storage_class = get_storage_class('openedxscorm.storage.S3ScormStorage')
